1dbf86ddd4e8756d6caf119ca99ca49b09ef06d9
Your Name <oluwatosin.olamilekan@54gene.com>
2019-10-04 23:50:04 +0100

    modified password reset and password reset db

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordReset extends Model
 {
-    //
+    protected $fillable = [
+        'email'
+    ];
+
+    protected $hidden =
+    [
+    	'token'
+    ];
 }
