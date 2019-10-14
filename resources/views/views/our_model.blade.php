7b6b5129fe4244225a2ce5edbedb0f640bdf0082
Your Name <oluwatosin.olamilekan@54gene.com>
2019-10-14 09:42:00 +0100

    random models show models picture

--- resources/views/views/our_model.blade.php
+++ resources/views/views/our_model.blade.php
@@ -7,21 +7,21 @@
             @foreach($random_model as $model)
             <figure class="ombre-carousel">
                 <!-- MODEL IMAGE -->
-                <a href="{{route('profile',$model->slug)}}" class="gallery link">
-                    @if(is_null($model->profile_image) && $model->sex == 'male')
-                        <img src="{{asset('frontend/images/maleunknown.jpg')}}" alt="" />
-
-                    @elseif(is_null($model->profile_image) && $model->sex == 'female')
-                        <img src="{{asset('frontend/images/ladyunknown.jpg')}}" alt="" />
-                    @else
-                    <img src="{{$model->profile_image}}" alt="" />
-                    @endif
+                @if($model->gender == 'male' && $model->profile_image === null )
+                    <img src="{{asset('frontend/images/maleunknow.jpg')}}" alt="" />
+                        @elseif($model->gender == 'female' && $model->profile_image === null)
+                                <img src="{{asset('frontend/images/ladyunknow.jpg')}}" alt="" />
+                        @elseif($model->profile_image !== null)
+                            <img src="{{$model->profile_image}}" alt="" />
+                @endif
                 </a>
                 <!-- MODEL INFO -->
                 <figcaption>
-                    <h5><a href="{{route('profile',$model->slug)}}">
+                    <h5>
+                        <a href="{{route('profile',$model->slug)}}">
                         {{$model->first_name}}
-                    </a></h5>
+                    </a>
+                    </h5>
                     {{-- <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p> --}}
                     <a class="arrow-button" href="{{route('profile',$model->slug)}}">View Profile</a>
                 </figcaption>
